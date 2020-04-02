7d3713936f7cf2888009237e8d04059705d8edfe
patrickmurray73 <g00344530@gmit.ie>
2020-04-02 19:55:20 +0100

    added addItemAtEnd()

--- StockManagement/Main.c
+++ StockManagement/Main.c
@@ -6,6 +6,7 @@
 #include "Database.h"
 
 void addItemAtStart(struct node** top);
+void addItemAtEnd(struct node* top);
 
 void main()
 {
@@ -28,7 +29,14 @@ void main()
 		switch (mode)
 		{
 			case 1:
-				addItemAtStart(&headPtr);
+				if (headPtr == NULL)
+				{
+					addItemAtStart(&headPtr);
+				}
+				else
+				{
+					addItemAtEnd(headPtr);
+				}
 				break;
 			case 2:
 				break;
@@ -103,4 +111,58 @@ void addItemAtStart(struct node** top)
 
 	newNode->NEXT = *top;
 	*top = newNode;
+}
+
+void addItemAtEnd(struct node* top)
+{
+	struct node* temp = top;
+	struct node* newNode = (struct node*)malloc(sizeof(struct node));
+
+	printf("\nPlease enter the following details");
+	printf("\nStock Item Number: ");
+	scanf("%d", &newNode->number);
+	printf("Stock Item Name: ");
+	scanf("%s", newNode->name);
+	printf("Stock Item Supplier Name: ");
+	scanf("%s", newNode->supplierName);
+	printf("Stock Item Supplier Contact Number: ");
+	scanf("%d", &newNode->supplierNumber);
+	printf("Re-order threshold limit: ");
+	scanf("%d", &newNode->thresholdLimit);
+	printf("Number of Units: ");
+	scanf("%d", &newNode->numOfUnits);
+	printf("Cost per Unit: ");
+	scanf("%f", &newNode->costPerUnit);
+	printf("Last Order Date(DDMMYYYY): ");
+	scanf("%d", &newNode->lastOrderDate);
+	printf("Does this item need to be stored in a hazardous chemical store?");
+	printf("\n1. Yes");
+	printf("\n2. No");
+	printf("\n=> ");
+	scanf("%d", &newNode->isHazardousChemical);
+	printf("Which Department does this stock item belong to?");
+	printf("\n1. Office");
+	printf("\n2. Maintenance");
+	printf("\n=> ");
+	scanf("%d", &newNode->department);
+	printf("Which of the following months do items need to re-order?");
+	printf("\n1. No Specified Month");
+	printf("\n2. Feb");
+	printf("\n3. August");
+	printf("\n=> ");
+	scanf("%d", &newNode->reOrderMonth);
+	printf("Which of the following people need to authorise the purchase?");
+	printf("\n1. Managing Director");
+	printf("\n2. Financial Controller");
+	printf("\n3. Department Manager");
+	printf("=> ");
+	scanf("%d", &newNode->authority);
+
+	while (temp->NEXT != NULL)
+	{
+		temp = temp->NEXT;
+	}
+
+	newNode->NEXT = NULL;
+	temp->NEXT = newNode;
 }
