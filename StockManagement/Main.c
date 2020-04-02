701a2c89301c549026b693e047c14c5296e89600
patrickmurray73 <g00344530@gmit.ie>
2020-04-02 17:25:07 +0100

    Added Menu

--- StockManagement/Main.c
+++ StockManagement/Main.c
@@ -6,6 +6,21 @@
 
 void main()
 {
+	int mode;
+
+	do
+	{
+		printf("\n1. Add Stock Item");
+		printf("\n2. Display All Stock Items");
+		printf("\n3. Display Stock Item");
+		printf("\n4. Update Stock Item");
+		printf("\n5. Delete Stock Item");
+		printf("\n6. Generate Statistics");
+		printf("\n7. Print All Stock Items To File");
+		printf("\n8. List Stock Items In Order Of Monetary Value");
+		printf("\nPlease enter mode: ");
+		scanf("%d", &mode);
+	} while (mode != -1);
 
 	_getch();
 }
