f88e7d09b889c054b77a9060a41b0d6e87bee466
patrickmurray73 <g00344530@gmit.ie>
2020-04-06 16:45:03 +0100

    showMenu() added

--- StockManagement/Main.c
+++ StockManagement/Main.c
@@ -1,17 +1,25 @@
 #include "Database.h"
 #include "Login.h"
 
+void showMenu(struct node* top, struct node** top2);
 
 void main()
 {
 	struct node* headPtr = NULL;
-	int mode;
 
 	initLogin();
 
 	init(&headPtr);
 	addEnd(headPtr);
 	
+	showMenu(headPtr, &headPtr);
+	
+}
+
+void showMenu(struct node* top, struct node* top2)
+{
+	int mode;
+
 	do {
 		printf("\n\n1. Add Stock Item");
 		printf("\n2. Display All Stock Items");
@@ -25,48 +33,47 @@ void main()
 		scanf("%d", &mode);
 
 		switch (mode) {
-			case 1:
-				if (headPtr == NULL) {
-					addItemAtStart(&headPtr);
-				} 
-				else {
-					addItemAtEnd(headPtr);
-				}
-				break;
-			case 2:
-				displayDatabase(headPtr);
-				break;
-			case 3:
-				displayItem(headPtr);
-				break;
-			case 4:
-				updateItem(headPtr);
-				break;
-			case 5:
-				if (headPtr == NULL) {
-					printf("Sorry the database is empty");
-				}
-				else {
-					deleteItem(headPtr, &headPtr);
-				}
-				break;
-			case 6:
-				generateStats(headPtr);
-				break;
-			case 7:
-				printToFile(headPtr);
-				break;
-			case 8:
-				stockInOrder(headPtr);
-				break;
-			case -1:
-				saveDatabase(headPtr);
-				printf("Goodbye.");
-				break;
-			default:
-				printf("\nInvalid Mode, please try again!");
-				break;
+		case 1:
+			if (top == NULL) {
+				addItemAtStart(&top2);
+			}
+			else {
+				addItemAtEnd(top);
+			}
+			break;
+		case 2:
+			displayDatabase(top);
+			break;
+		case 3:
+			displayItem(top);
+			break;
+		case 4:
+			updateItem(top);
+			break;
+		case 5:
+			if (top == NULL) {
+				printf("Sorry the database is empty");
+			}
+			else {
+				deleteItem(top, &top2);
+			}
+			break;
+		case 6:
+			generateStats(top);
+			break;
+		case 7:
+			printToFile(top);
+			break;
+		case 8:
+			stockInOrder(top);
+			break;
+		case -1:
+			saveDatabase(top);
+			printf("Goodbye.");
+			break;
+		default:
+			printf("\nInvalid Mode, please try again!");
+			break;
 		}
-
 	} while (mode != -1);
 }
