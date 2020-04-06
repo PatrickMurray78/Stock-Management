666a9f26b01485acc4594b84f5ea447aec8530b0
patrickmurray73 <g00344530@gmit.ie>
2020-04-06 18:10:51 +0100

    Formatted code

--- StockManagement/Login.c
+++ StockManagement/Login.c
@@ -18,26 +18,22 @@ void login(Login credentials)
 	char username[15];
 	char password[20];
 
-	do
-	{
+	do {
 		printf("Username: ");
 		scanf("%s", username);
 
-		if (strcmp(credentials.username, username) == 0)
-		{
+		if (strcmp(credentials.username, username) == 0) {
 			printf("Password: ");
 			int i = 0;
 			do {
 				password[i] = getch();
-				if (password[i] != '\r')
-				{
+				if (password[i] != '\r') {
 					printf("*");
 				}
 				i++;
 			} while (password[i - 1] != '\r');
 			password[i - 1] = '\0';
-			if (strcmp(credentials.password, password) == 0)
-			{
+			if (strcmp(credentials.password, password) == 0) {
 				loginSuccess = 1;
 			}
 			else {
@@ -48,4 +44,4 @@ void login(Login credentials)
 			printf("\nInvalid Username!\n\n");
 		}
 	} while (loginSuccess == 0);
-}
+} 
