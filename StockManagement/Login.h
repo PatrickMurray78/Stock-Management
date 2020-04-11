b7ae275b3fe65dd02b262f8ff82a70c9e12b0ab0
patrickmurray73 <g00344530@gmit.ie>
2020-04-11 16:36:57 +0100

    Added comments

--- StockManagement/Login.h
+++ StockManagement/Login.h
@@ -3,12 +3,14 @@
 
 #include<stdio.h>
 
+// Declare struct
 typedef struct {
 	char username[15];
 	char password[8];
 	struct node* NEXT;
 }Login;
 
+// Initialise functions
 void initLogin();
 void login(Login* top);
 
